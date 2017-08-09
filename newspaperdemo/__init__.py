f0a0a254ca20bfc0dae49de2664e9ca03cf031fa
selvakarthik21 <selvakarthik21@users.noreply.github.com>
2017-08-09 08:28:35 +0000

    Update __init__.py

--- newspaperdemo/__init__.py
+++ newspaperdemo/__init__.py
@@ -16,39 +16,52 @@ except:
     _, ex, _ = sys.exc_info()
     log.error(ex.message)
 
-@app.route('/')
+
 def index():
     return render_template('index.html')
 
+@app.route('/')
 @app.route('/articles/show')
 def show_article():
     url_to_clean = request.args.get('url_to_clean')
     if not url_to_clean:
-        return redirect(url_for('index'))
+	return jsonify(
+		'authors'= '', 
+		'title'= '',
+		'text'= '',
+		'keywords'= '',
+		'summary'= ''
+	)
+    else:
+    	article = Article(url_to_clean)
+	article.download()
+	article.parse()
 
-    article = Article(url_to_clean)
-    article.download()
-    article.parse()
+	try:
+	  html_string = ElementTree.tostring(article.clean_top_node)
+	except:
+	  html_string = "Error converting html to string."
 
-    try:
-      html_string = ElementTree.tostring(article.clean_top_node)
-    except:
-      html_string = "Error converting html to string."
+	try:
+	  article.nlp()
+	except:
+	  log.error("Couldn't process with NLP")
 
-    try:
-      article.nlp()
-    except:
-      log.error("Couldn't process with NLP")
-
-    a = {
-          'html': html_string, 
-         'authors': str(', '.join(article.authors)), 
-         'title': article.title,
-         'text': article.text,
-         'top_image': article.top_image,
-         'videos': str(', '.join(article.movies)),
-         'keywords': str(', '.join(article.keywords)),
-         'summary': article.summary
-         }
-    return render_template('article/index.html', article=a, url=url_to_clean)
+	a = {
+		  'html': html_string, 
+		 'authors': str(', '.join(article.authors)), 
+		 'title': article.title,
+		 'text': article.text,
+		 'top_image': article.top_image,
+		 'videos': str(', '.join(article.movies)),
+		 'keywords': str(', '.join(article.keywords)),
+		 'summary': article.summary
+		 }
+        return jsonify(
+		'authors'= str(', '.join(article.authors)), 
+		'title'= article.title,
+		'text'= article.text,
+		'keywords'= str(', '.join(article.keywords)),
+		'summary'= article.summary
+	)
     
